82643e03b10cab8560ddb4b0190df1ee24d63a2e
Mark-Vale <markpvale@gmail.com>
2022-01-11 18:39:10 -0500

    add feature flag

--- app.ts
+++ app.ts
@@ -9,35 +9,50 @@ interface LocationWithTimezone {
   utcOffset: number;
 };
 
-const getLocationsWithTimezones = (request: Request, response: Response, next: NextFunction) => {
-  let locations: LocationWithTimezone[] = [
-    {
-      location: 'Germany',
-      timezoneName: 'Central European Time',
-      timezoneAbbr: 'CET',
-      utcOffset: 1
-    },
-    {
-      location: 'China',
-      timezoneName: 'China Standard Time',
-      timezoneAbbr: 'CST',
-      utcOffset: 8
-    },
-    {
-      location: 'Argentina',
-      timezoneName: 'Argentina Time',
-      timezoneAbbr: 'ART',
-      utcOffset: -3
-    },
-    {
-      location: 'Japan',
-      timezoneName: 'Japan Standard Time',
-      timezoneAbbr: 'JST',
-      utcOffset: 9
-    }
-  ];
+const showLocations = false;
 
-  response.status(200).json(locations);
+let locations: LocationWithTimezone[] = [
+  {
+    location: 'Germany',
+    timezoneName: 'Central European Time',
+    timezoneAbbr: 'CET',
+    utcOffset: 1
+  },
+  {
+    location: 'China',
+    timezoneName: 'China Standard Time',
+    timezoneAbbr: 'CST',
+    utcOffset: 8
+  },
+  {
+    location: 'Argentina',
+    timezoneName: 'Argentina Time',
+    timezoneAbbr: 'ART',
+    utcOffset: -3
+  },
+  {
+    location: 'Japan',
+    timezoneName: 'Japan Standard Time',
+    timezoneAbbr: 'JST',
+    utcOffset: 9
+  },
+  {
+    location: 'New York',
+    timezoneName: 'Eastern Standard Time',
+    timezoneAbbr: 'EST',
+    utcOffset: -5
+  }
+];
+
+
+
+const getLocationsWithTimezones = (request: Request, response: Response, next: NextFunction) => {
+
+  if (showLocations) {
+    response.status(200).json(locations);
+  } else {
+    response.status(500).json([{message: 'A different message'}])
+  }
 
 };
 
